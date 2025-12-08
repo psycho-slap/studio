03b8bafd7bb23865933f0ede2bad72e5f8199b87
peach slap (peachslap) <gaibadyllin363@gmail.com>
2025-12-08 18:55:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5148384623-83e0a",
+  "appId": "1:649113811654:web:134ec5a8e70a1382fc35c7",
+  "apiKey": "AIzaSyB4oo7ydznipTWw9Y1vXi-qYYZ9mq2WZgU",
+  "authDomain": "studio-5148384623-83e0a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "649113811654"
+};
